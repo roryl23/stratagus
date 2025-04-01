18ff3a0bb8f7eb55c2e1378f2beef0fb36da2300
Rory Linehan <rorylinehan@protonmail.com>
2025-04-01 12:32:42 -0500

    revert

--- src/stratagus/script.cpp
+++ src/stratagus/script.cpp
@@ -2019,9 +2019,11 @@ void InitLua()
 	static const luaL_Reg lualibs[] = {
 		{"", luaopen_base},
 		{LUA_TABLIBNAME, luaopen_table},
-		{LUA_IOLIBNAME, luaopen_io},
+#ifdef DEBUG
 		{LUA_LOADLIBNAME, luaopen_package},
+#endif
 		{LUA_OSLIBNAME, luaopen_os},
+		{LUA_IOLIBNAME, luaopen_io},
 		{LUA_STRLIBNAME, luaopen_string},
 		{LUA_MATHLIBNAME, luaopen_math},
 		{LUA_DBLIBNAME, luaopen_debug},
